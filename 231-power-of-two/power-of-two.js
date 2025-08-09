d81736bd3273c1fb4d1c04b56566042c1698d311
Vinay Somawat <vinaysomawat40@gmail.com>
2025-08-09 09:22:27 +0530

    Time: 0 ms (100.00%) | Memory: 57.2 MB (47.34%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,6 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if(n<0) return false;
-    return (n >>> 0 ).toString(2).split('').filter(x => x==='1').length === 1;
+    return n > 0 && (n & (n - 1)) === 0;
 };
