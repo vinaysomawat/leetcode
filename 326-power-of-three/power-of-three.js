1556cb8e8b54980032e7ca896493393206a85c33
Vinay Somawat <vinaysomawat40@gmail.com>
2025-08-13 09:42:09 +0530

    Time: 5 ms (84.23%) | Memory: 64.1 MB (97.47%) - LeetSync

--- 326-power-of-three/power-of-three.js
+++ 326-power-of-three/power-of-three.js
@@ -3,12 +3,10 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    let pow = 0;
-
-    while(true) {
-        if(n=== Math.pow(3, pow)) return true;
-        if(n<Math.pow(3, pow)) return false;
-
-        pow++;
+    if(n<=0) return false;
+    while(n%3===0) {
+        n /=3;
     }
+
+    return n===1;
 };
