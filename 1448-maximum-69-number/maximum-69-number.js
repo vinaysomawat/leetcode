c60ed0b6594b99094387b2e3c2981f14771a2c69
Vinay Somawat <vinaysomawat40@gmail.com>
2025-08-16 09:06:52 +0530

    Time: 5 ms (2.60%) | Memory: 54.8 MB (6.06%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    const newNum = num.toString().split('');
+    for(let i=0;i<newNum.length;i++) {
+        console.log(newNum[i],newNum);
+        if(newNum[i]=='6') {
+            newNum[i]= '9';
+            break;
+        }
+    }
+    return parseInt(newNum.join(''));
+};
