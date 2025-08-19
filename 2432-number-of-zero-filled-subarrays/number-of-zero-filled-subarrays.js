30968c8758f3c022dd63b5c5bbe85ba601aea340
Vinay Somawat <vinaysomawat40@gmail.com>
2025-08-19 09:41:53 +0530

    Time: 8 ms (29.47%) | Memory: 70.8 MB (17.89%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.js
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.js
@@ -3,27 +3,17 @@
  * @return {number}
  */
 var zeroFilledSubarray = function(nums) {
-    let count =0;
-    let res = [];
-    for(let i=0;i<nums.length;i++) {
-        if(nums[i]!=0) {
-            res.push(count);
-            count=0;
-        } else {
+    let count = 0;
+    let result = 0;
+
+    for (let num of nums) {
+        if (num === 0) {
             count++;
+            result += count;
+        } else {
+            count = 0;
         }
     }
-    res.push(count);
-    let max = Math.max(...res);
-    let dp = [];
-    dp[0] = 0;
-    for(let i=1;i<=max;i++) {
-        dp[i] = dp[i-1]+i;
-    }
-    // console.log(res, max, dp);
-    let result = 0;
-    res.forEach((item) => {
-        result += dp[item];
-    })
+
     return result;
 };
