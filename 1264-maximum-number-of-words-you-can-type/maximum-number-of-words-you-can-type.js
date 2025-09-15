476d1f5bdd2032c4aed982334ee2590604a0e856
Vinay Somawat <vinaysomawat40@gmail.com>
2025-09-15 10:39:54 +0530

    Time: 4 ms (41.67%) | Memory: 57.8 MB (10.00%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.js
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.js
@@ -4,16 +4,19 @@
  * @return {number}
  */
 var canBeTypedWords = function(text, brokenLetters) {
+    const brokenSet = new Set(brokenLetters);
     let count = 0;
-    text.split(' ').forEach(item => {
-        if(check(brokenLetters, item)) count++;
-    });
+
+    for (const word of text.split(' ')) {
+        let valid = true;
+        for (const ch of word) {
+            if (brokenSet.has(ch)) {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) count++;
+    }
+
     return count;
 };
-
-function check(br, str) {
-    for(let i=0;i<str.length;i++) {
-        if(br.includes(str[i])) return false;
-    }
-    return true;
-}
